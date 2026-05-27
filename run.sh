ca826d983e2fe39e5de52f45e5d3ddd6c3bcab55
Anh.Nguyen <salebloomstore@outlook.com>
2026-05-27 16:25:05 +0700

    first commit

--- run.sh
+++ run.sh
@@ -1,1 +1,74 @@
-tail -f /dev/null
+#!/bin/bash
+
+# =====================================================
+# NUXTJS BOOTSTRAP SCRIPT
+# =====================================================
+
+set -e
+source /var/www/cache/.env
+
+cd /var/www
+
+echo "🚀 Checking NuxtJS project..."
+
+# =====================================================
+# CREATE PROJECT FIRST TIME
+# =====================================================
+if [ ! -f app/package.json ]; then
+  echo "📦 Creating Nuxt project..."
+
+  npm create nuxt@latest app <<EOF
+
+EOF
+
+  cd app
+
+  echo "📄 Creating env..."
+
+#   cat > .env << EOF
+# NESTJS_WEBAPI_IO_DATA=${NESTJS_WEBAPI_IO_DATA}
+# EOF
+
+  echo "📄 Fixing nuxt.config..."
+
+#   cat > nuxt.config.ts << 'EOF'
+# export default defineNuxtConfig({
+#   ssr: true,
+
+#   runtimeConfig: {
+#     public: {
+#       apiBase: process.env.NESTJS_WEBAPI_IO_DATA
+#     }
+#   }
+# })
+# EOF
+
+  echo "✅ Project created"
+fi
+
+
+# =========================
+# ALWAYS ENSURE ENV
+# =========================
+
+cd /var/www/app
+
+# cat > .env << EOF
+# NESTJS_WEBAPI_IO_DATA=${NESTJS_WEBAPI_IO_DATA}
+# EOF
+
+
+# =========================
+# BUILD + RUN
+# =========================
+
+echo "📦 Installing dependencies..."
+npm install
+
+echo "📦 Building project..."
+
+npm run build
+
+echo "🚀 Starting NuxtJS..."
+
+npm run start:prod
