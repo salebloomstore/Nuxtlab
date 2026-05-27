82a3ed9ded96f3769a7b77db9c8c3f474e8717c3
Anh.Nguyen <salebloomstore@outlook.com>
2026-05-27 19:30:06 +0700

    first commit

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ echo "📦 Installing dependencies..."
 
 # TypeScript toolchain
 npm install -D typescript@5.9.3 ts-node@10.9.2 @types/node@24.12.4
-npm install nuxt@latest
+npm install nuxt@4.4.6
 
 npm install
 
