4362e738f79180718d3f20e757b95cbc2543eda6
Anh.Nguyen <salebloomstore@outlook.com>
2026-05-27 17:58:49 +0700

    first commit

--- run.sh
+++ run.sh
@@ -7,24 +7,29 @@
 set -e
 source /var/www/cache/.env
 
+# =========================
+# ALWAYS ENSURE ENV
+# =========================
+
 cd /var/www/app
 
 # cat > .env << EOF
 # NESTJS_WEBAPI_IO_DATA=${NESTJS_WEBAPI_IO_DATA}
 # EOF
 
-
 # =========================
-# BUILD + RUN
+# BUILD
 # =========================
 
 echo "📦 Installing dependencies..."
 npm install
 
 echo "📦 Building project..."
-
 npm run build
 
-echo "🚀 Starting NuxtJS..."
+# =========================
+# RUN
+# =========================
 
+echo "🚀 Starting NuxtJS..."
 node .output/server/index.mjs
