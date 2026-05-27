32a3083af9bf657785f101dff1c48d75fdcfcaa9
Anh.Nguyen <salebloomstore@outlook.com>
2026-05-27 17:44:44 +0700

    first commit

--- run.sh
+++ run.sh
@@ -3,51 +3,9 @@
 # =====================================================
 # NUXTJS BOOTSTRAP SCRIPT
 # =====================================================
-
+# tail -f /dev/null
 set -e
 source /var/www/cache/.env
-
-cd /var/www
-
-echo "🚀 Checking NuxtJS project..."
-
-# =====================================================
-# CREATE PROJECT FIRST TIME
-# =====================================================
-if [ ! -f app/package.json ]; then
-  echo "📦 Creating Nuxt project..."
-
-  printf "y\nui\n0\n0\ny\nn\n" | npm create nuxt@latest app
-
-  cd app
-
-  echo "📄 Creating env..."
-
-#   cat > .env << EOF
-# NESTJS_WEBAPI_IO_DATA=${NESTJS_WEBAPI_IO_DATA}
-# EOF
-
-  echo "📄 Fixing nuxt.config..."
-
-#   cat > nuxt.config.ts << 'EOF'
-# export default defineNuxtConfig({
-#   ssr: true,
-
-#   runtimeConfig: {
-#     public: {
-#       apiBase: process.env.NESTJS_WEBAPI_IO_DATA
-#     }
-#   }
-# })
-# EOF
-
-  echo "✅ Project created"
-fi
-
-
-# =========================
-# ALWAYS ENSURE ENV
-# =========================
 
 cd /var/www/app
 
