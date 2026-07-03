6dfcc9d58c64034c23085774f80197adc918ea5c
Anh.Nguyen <salebloomstore@outlook.com>
2026-07-03 09:49:27 +0700

    first commit

--- server/master/run.sh
+++ server/master/run.sh
@@ -3,4 +3,21 @@
 # =====================================================
 # NUXTJS BOOTSTRAP SCRIPT
 # =====================================================
-tail -f /dev/null
+# tail -f /dev/null
+
+# =========================
+# BUILD
+# =========================
+
+echo "📦 Installing dependencies..."
+npm ci
+
+echo "📦 Building project..."
+npm run build
+
+# =========================
+# RUN
+# =========================
+
+echo "🚀 Starting NestJS..."
+npm run start:prod
